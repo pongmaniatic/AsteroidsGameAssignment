055650f91650ec765231f6529a0bdf5fbcceab8f
pongmaniatic <37563473+pongmaniatic@users.noreply.github.com>
2021-02-26 14:58:58 +0100

    clean up of the project

---
+++ AsteroidsAssignment/src/Game.cpp
@@ -0,0 +1,123 @@
+//
+// Created by Vilhelm Stokstad on 2021-02-17.
+//
+
+
+#include "Game.h"
+#include "Player.h"
+SDL_Texture* playerTex;
+SDL_Rect srcR, destR;
+
+Game::Game()
+{
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+
+	_screenSize = new Vector2Int(800, 600);
+
+	player = Player();
+
+	SDL_CreateWindowAndRenderer(_screenSize->x, _screenSize->y, SDL_WINDOW_ALLOW_HIGHDPI, &_window, &_renderer);
+
+	if (_window == nullptr) {
+		// In the case that the window could not be made...
+		std::cout << "Could not create window: " << SDL_GetError() << std::endl;
+
+	}
+
+	SDL_SetRenderDrawColor(_renderer, 100, 100, 100, 250);
+	SDL_RenderClear(_renderer);
+
+	SDL_RenderPresent(_renderer);
+
+	appRunning = true;
+
+	SDL_Surface* tmpSurface = IMG_Load("assets/playeWhite.png");
+	playerTex = SDL_CreateTextureFromSurface(_renderer, tmpSurface);
+	SDL_FreeSurface(tmpSurface);
+
+}
+
+
+void Game::GameLoop()
+{
+
+
+	while (appRunning) {
+		//Check for input;
+		HandleEvents();
+
+		//Render the screen
+		Render();
+
+
+	}
+
+}
+
+
+void Game::HandleEvents()
+{
+	//Colors just for fun//
+	Uint8 r;
+	Uint8 g;
+	Uint8 b;
+	Uint8 a;
+//get the current color//
+	SDL_GetRenderDrawColor(_renderer, &r, &g, &b, &a);
+
+	while (SDL_PollEvent(&_events)) {
+		if (_events.type==SDL_KEYDOWN){
+		player.Input(_events);
+		}
+		if (_events.button.type == SDL_MOUSEBUTTONDOWN) {
+			r = _events.button.x;
+			b = _events.button.y;
+			SDL_SetRenderDrawColor(_renderer, r, g, b, a);
+			//Set and render a new color//
+			SDL_RenderClear(_renderer);
+		}
+		//check if app is closing to quit.
+		if (SDL_QUIT == _events.type) {
+			std::cout << "SDL_QUIT" << std::endl;
+			SDL_Log("Program quit after %i ticks", _events.quit.timestamp);
+			appRunning = false;
+
+		}
+	}
+
+
+}
+
+void Game::Render()
+{
+
+
+
+	SDL_RenderCopy(_renderer, playerTex, NULL, NULL);
+
+
+	SDL_RenderPresent(_renderer);
+
+
+}
+void Game::Update()
+{
+	cnt++;
+
+
+
+}
+
+
+void Game::Cleanup()
+{
+
+	SDL_DestroyRenderer(_renderer);
+	SDL_DestroyWindow(_window);
+	SDL_WasInit(SDL_INIT_EVERYTHING);
+	SDL_Quit();
+
+}
+
